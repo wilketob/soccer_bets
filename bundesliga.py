050d1860034dcc2d732c9ec6128f8b894b058004
Tob <wilketob@gmail.com>
2019-02-18 11:48:22 +0100

    defining routines

--- bundesliga.py
+++ bundesliga.py
@@ -1,27 +1,31 @@
 import re
 from urllib import request
 
-url = 'https://www.fussballdaten.de/bundesliga/kreuztabelle/'
+url_crosstable = 'https://www.fussballdaten.de/bundesliga/kreuztabelle/'
 
-url_requested = request.urlopen(url)
-html_content = str(url_requested.read())
-
-
-print(url_requested.code)
-print(html_content)
-page_content=[]
-
-re_search_chunk = []
-
-re_search_chunk = re.findall('>[0-9]:[0-9]<',html_content)
+def data_crosstable(url_crosstable):
+    url_requested = request.urlopen(url_crosstable)
+    html_content = str(url_requested.read())
+    return html_content
+    #print(url_requested.code)
+    #print(html_content)
 
 
-print(re_search_chunk.sort())
-#print(type(re_search_chunk))
-a_set = set(re_search_chunk)
-for e in a_set:
-    print(e.strip('>').strip('<') + ' = ' + str(re_search_chunk.count(e)))
+def score_results():
+    re_search_chunk = []
+    re_search_chunk = re.findall('>[0-9]:[0-9]<', data_crosstable(url_crosstable))
+    return re_search_chunk
 
+def main():
+    score_results()
+    print(re_search_chunk.sort())
+    #print(type(re_search_chunk))
+    set_search_chunk = set(re_search_chunk)
+    for a in set_search_chunk:
+        print(a.strip('>').strip('<') + ' = ' + str(re_search_chunk.count(e)))
+
+if __name__ == '__main__':
+    main()
 
 #URL für Tabellenstände an einem bestimmten Spieltag: https://www.fussballdaten.de/bundesliga/tabelle/1977/2/  (Jahr 2018 = Saison 2016/2017 // 2 = Spieltag)
 # URL für Spielergebnisse an einem bestimmten Tag mit Angabe des Tages https://www.fussballdaten.de/bundesliga/2017/4/
